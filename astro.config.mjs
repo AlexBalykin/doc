f39a638e1a96c99bea88a14184a9c2b404f60d05
Alex Baly <alex@example.com>
2025-09-15 15:58:55 +0300

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-	site: 'https://AlexBalykin.gitlab.io',
+	site: 'https://AlexBalykin.github.io',
 	base: '/',
   integrations: [
     starlight({
