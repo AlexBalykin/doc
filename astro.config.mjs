e865a8c6a3a47a62c0fabde3b1e331ac7706954d
Alex Baly <alex@example.com>
2025-09-15 16:54:30 +0300

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  base: '/doc/', // Указываем имя репозитория с косой чертой
+  site: 'https://alexbalykin.github.io/doc', // URL сайта на GitHub Pages
   integrations: [
     starlight({
       title: 'TapBank',
